7288bfcd27bbc001b0f9c8dc81c3d257fad88060
Shannon Vaz <nonnahs.vaz@gmail.com>
2024-03-15 10:05:41 +0000

    Generate a short poem

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Langchain Tutorial")
+from langchain.llms import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+llm = OpenAI(
+    openai_api_key=openai_api_key
+)
+result = llm("Write a very very short poem")
+print(result)
