87a47cb3abf9aa6735ec060d3febf1f6e0a4ad35
Shannon Vaz <nonnahs.vaz@gmail.com>
2024-03-15 10:14:14 +0000

    Add prompt template

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-openai_api_key = os.getenv('OPENAI_API_KEY')
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
-llm = OpenAI(
-    openai_api_key=openai_api_key
+llm = OpenAI(openai_api_key=openai_api_key)
+code_prompt = PromptTemplate(
+    template="Write a very short {language} function that will {task}",
+    input_variables=["language", "task"],
 )
-result = llm("Write a very very short poem")
-print(result)
+code_chain = LLMChain(llm=llm, prompt=code_prompt)
+result = code_chain({
+    "language": "python",
+    "task": "return a list of numbers",
+})
+print(result["text"])
