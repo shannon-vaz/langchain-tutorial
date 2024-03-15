c3093f80f82cffe77f2bd563e3a49d0bf1f11247
Shannon Vaz <nonnahs.vaz@gmail.com>
2024-03-15 10:21:59 +0000

    Get chain inputs from cli

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 import os
+import argparse
 from dotenv import load_dotenv
 
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--task", default="return a list of numbers")
+parser.add_argument("--language", default="python")
+args = parser.parse_args()
 
 llm = OpenAI(openai_api_key=openai_api_key)
 code_prompt = PromptTemplate(
@@ -14,7 +20,7 @@ code_prompt = PromptTemplate(
 )
 code_chain = LLMChain(llm=llm, prompt=code_prompt)
 result = code_chain({
-    "language": "python",
-    "task": "return a list of numbers",
+    "language": args.language,
+    "task": args.task,
 })
 print(result["text"])
