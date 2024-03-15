5b18ac6891b3e4b352ca1d5b78fbe4f0116b11c3
Shannon Vaz <nonnahs.vaz@gmail.com>
2024-03-15 12:09:03 +0000

    Add chat prompt template

--- tchat/main.py
+++ tchat/main.py
@@ -1,4 +1,22 @@
+import os
+from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+
+load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+chat = ChatOpenAI(openai_api_key=openai_api_key)
+
+prompt = ChatPromptTemplate(
+    input_variables=["content"],
+    messages=[HumanMessagePromptTemplate.from_template("{content}")],
+)
+
+chain = LLMChain(llm=chat, prompt=prompt)
 
 while True:
     content = input(">> ")
-    print(f"You said: {content}")
+    result = chain({"content": content})
+    print(result["text"])
