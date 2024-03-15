f32bb1cc392bd57f4ba9168e919cb3dd5b275a0a
Shannon Vaz <nonnahs.vaz@gmail.com>
2024-03-15 11:20:19 +0000

    Add second chain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 import os
 import argparse
 from dotenv import load_dotenv
@@ -15,12 +15,30 @@ args = parser.parse_args()
 
 llm = OpenAI(openai_api_key=openai_api_key)
 code_prompt = PromptTemplate(
-    template="Write a very short {language} function that will {task}",
     input_variables=["language", "task"],
+    template="Write a very short {language} function that will {task}",
 )
-code_chain = LLMChain(llm=llm, prompt=code_prompt)
-result = code_chain({
-    "language": args.language,
-    "task": args.task,
-})
-print(result["text"])
+test_prompt = PromptTemplate(
+    input_variables=["language", "code"],
+    template="Write a test for the following code in {language}:\n{code}",
+)
+code_chain = LLMChain(llm=llm, prompt=code_prompt, output_key="code")
+test_chain = LLMChain(llm=llm, prompt=test_prompt, output_key="test")
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["language", "task"],
+    output_variables=["code", "test"],
+)
+
+result = chain(
+    {
+        "language": args.language,
+        "task": args.task,
+    }
+)
+
+print("-" * 80)
+print(">" * 10, "Code:")
+print(result["code"])
+print(">" * 10, "Test:")
+print(result["test"])
