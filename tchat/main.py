67f4470cd409f9f57704d34953339838592f50cc
Shannon Vaz <nonnahs.vaz@gmail.com>
2024-03-18 14:07:23 +0530

    Replace conversation buffer memory with conversation summary memory

--- tchat/main.py
+++ tchat/main.py
@@ -7,19 +7,24 @@ from langchain.prompts import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
 )
-from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.memory import ConversationSummaryMemory, ConversationBufferMemory, FileChatMessageHistory
 
 load_dotenv()
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 chat = ChatOpenAI(openai_api_key=openai_api_key)
-memory = ConversationBufferMemory(
-    chat_memory=FileChatMessageHistory(
-        file_path="chat_history.json",
-    ),
+memory = ConversationSummaryMemory(
     memory_key="messages",
     return_messages=True,
+    llm=chat,
 )
+# memory = ConversationBufferMemory(
+#     chat_memory=FileChatMessageHistory(
+#         file_path="chat_history.json",
+#     ),
+#     memory_key="messages",
+#     return_messages=True,
+# )
 
 prompt = ChatPromptTemplate(
     input_variables=["content", "messages"],
