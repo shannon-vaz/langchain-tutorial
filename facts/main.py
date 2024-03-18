2ccd149d5426562d516420c4502ddd9ed050a0f6
Shannon Vaz <nonnahs.vaz@gmail.com>
2024-03-18 17:56:30 +0530

    Generate and store embeddings using ChromaDB

--- facts/main.py
+++ facts/main.py
@@ -2,9 +2,13 @@ import os
 from dotenv import load_dotenv
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
+
+embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
 
 text_splitter = CharacterTextSplitter(
     separator="\n",
@@ -15,6 +19,12 @@ text_splitter = CharacterTextSplitter(
 loader = TextLoader("facts.txt")
 docs = loader.load_and_split(text_splitter=text_splitter)
 
-for doc in docs:
-    print(doc.page_content)
+db = Chroma.from_documents(docs, embedding=embeddings, persist_directory="emb")
+
+query = "What is an interesting fact about the English language?"
+results = db.similarity_search_with_score(query)
+
+for result in results:
     print("\n")
+    print(result[1])
+    print(result[0].page_content)
