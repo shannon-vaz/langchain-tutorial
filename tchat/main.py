fe874aef1317d2f9755387320e7f9bb2d4ba1b6a
Shannon Vaz <nonnahs.vaz@gmail.com>
2024-03-18 13:45:52 +0530

    Add conversation memory to chatbot

--- tchat/main.py
+++ tchat/main.py
@@ -2,19 +2,28 @@ import os
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
-from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain.prompts import (
+    MessagesPlaceholder,
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.memory import ConversationBufferMemory
 
 load_dotenv()
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 chat = ChatOpenAI(openai_api_key=openai_api_key)
+memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
 
 prompt = ChatPromptTemplate(
-    input_variables=["content"],
-    messages=[HumanMessagePromptTemplate.from_template("{content}")],
+    input_variables=["content", "messages"],
+    messages=[
+        MessagesPlaceholder(variable_name="messages"),
+        HumanMessagePromptTemplate.from_template("{content}"),
+    ],
 )
 
-chain = LLMChain(llm=chat, prompt=prompt)
+chain = LLMChain(llm=chat, prompt=prompt, memory=memory)
 
 while True:
     content = input(">> ")
