2febe56338c483f7bb980f871bfead7e36885840
Shannon Vaz <nonnahs.vaz@gmail.com>
2024-03-18 17:31:58 +0530

    Load and split text into chunks

--- facts/main.py
+++ facts/main.py
@@ -1,5 +1,20 @@
 import os
 from dotenv import load_dotenv
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
 
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
+
+text_splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=200,
+    chunk_overlap=0,
+)
+
+loader = TextLoader("facts.txt")
+docs = loader.load_and_split(text_splitter=text_splitter)
+
+for doc in docs:
+    print(doc.page_content)
+    print("\n")
