e627f348280d5a310b0a2123d2a2a16c5a16fe22
Shannon Vaz <nonnahs.vaz@gmail.com>
2024-03-18 13:54:11 +0530

    Store chat history in file

--- tchat/main.py
+++ tchat/main.py
@@ -7,13 +7,19 @@ from langchain.prompts import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
 )
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
 
 load_dotenv()
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 chat = ChatOpenAI(openai_api_key=openai_api_key)
-memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+memory = ConversationBufferMemory(
+    chat_memory=FileChatMessageHistory(
+        file_path="chat_history.json",
+    ),
+    memory_key="messages",
+    return_messages=True,
+)
 
 prompt = ChatPromptTemplate(
     input_variables=["content", "messages"],
