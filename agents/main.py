2f02e128c140bcc974b34698acdac3acd9cb749b
Shannon Vaz <nonnahs.vaz@gmail.com>
2024-03-19 12:40:53 +0000

    Add custom handler for logging messages sent to LLM

--- agents/main.py
+++ agents/main.py
@@ -12,11 +12,13 @@ from langchain.memory import ConversationBufferMemory
 
 from tools.sql import list_tables, run_query_tool, describe_tables_tool
 from tools.report import write_report_tool
+from handlers.chat_model_start_handler import ChatModelStartHandler
 
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
-chat = ChatOpenAI(openai_api_key=openai_api_key)
+handler = ChatModelStartHandler()
+chat = ChatOpenAI(openai_api_key=openai_api_key, callbacks=[handler])
 
 tables = list_tables()
 print(f"Tables:\n{tables}")
@@ -43,10 +45,10 @@ memory = ConversationBufferMemory(memory_key="chat_history", return_messages=Tru
 
 agent = OpenAIFunctionsAgent(llm=chat, prompt=prompt, tools=tools)
 
-agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools, memory=memory)
+agent_executor = AgentExecutor(agent=agent, tools=tools, memory=memory)
 
 # human_query = "how many users are in the database?"
 # human_query = "How many users have provided an address?"
 human_query = "How many orders are there? Write the results to an html report"
 agent_executor(human_query)
-agent_executor("Repeat the exact process for users.")
+# agent_executor("Repeat the exact process for users.")
