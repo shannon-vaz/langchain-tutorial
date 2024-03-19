ad5bf8b508f1e3a3a67133131d5fd41ba9b9e3b7
Shannon Vaz <nonnahs.vaz@gmail.com>
2024-03-19 11:40:48 +0530

    Create custom retriever to remove duplicate docs from query result

--- facts/prompt.py
+++ facts/prompt.py
@@ -4,6 +4,8 @@ from langchain.chat_models import ChatOpenAI
 from langchain.vectorstores.chroma import Chroma
 from langchain.chains import RetrievalQA
 from langchain.embeddings import OpenAIEmbeddings
+from redundant_filter_retriever import RedundantFilterRetriever
+
 
 load_dotenv()
 
@@ -16,7 +18,7 @@ db = Chroma(
     persist_directory="emb",
     embedding_function=embeddings,
 )
-retriever = db.as_retriever()
+retriever = RedundantFilterRetriever(embeddings=embeddings, chroma=db)
 
 chain = RetrievalQA.from_chain_type(
     llm=chat,
---
+++ facts/redundant_filter_retriever.py
@@ -0,0 +1,19 @@
+from typing import List
+from langchain.embeddings.base import Embeddings
+from langchain.vectorstores.chroma import Chroma
+from langchain.schema import BaseRetriever
+from langchain_core.documents import Document
+
+
+class RedundantFilterRetriever(BaseRetriever):
+    embeddings: Embeddings
+    chroma: Chroma
+
+    def _get_relevant_documents(self, query: str, **kwargs) -> List[Document]:
+        # Calculate embeddings for the query
+        emb = self.embeddings.embed_query(query)
+
+        # Filter out redundant documents using max marginal relevance
+        return self.chroma.max_marginal_relevance_search_by_vector(
+            embedding=emb, lambda_mult=0.8
+        )
