592e0e73e1af877138542b6aca8f03cbcfb1aa73
Shannon Vaz <nonnahs.vaz@gmail.com>
2024-03-24 19:02:30 +0530

    Add conversation based retrieval QA chain

---
+++ langchain-pdf/app/chat/llms/chatopenai.py
@@ -0,0 +1,8 @@
+import os
+from langchain.chat_models import ChatOpenAI
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+
+def build_llm(chat_args):
+    return ChatOpenAI(openai_api_key=openai_api_key)
